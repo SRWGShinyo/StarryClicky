130054c0e21ac214e7aaee6612a4406940aba9cb
Yohann Degli Esposti <yohann.degliesposti@gmail.com>
2021-01-23 13:30:16 +0100

    Stary: a v1 model is up.

---
+++ Assets/Scripts/PointNClick/PointNClickManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointNClickManager : MonoBehaviour
+{
+    public enum CLICKERSTATE
+    {
+        IDLE,
+        OBSERVE,
+        USE,
+        PICKUP,
+        TALK,
+        MOVE,
+        MENU
+    }
+
+    public CLICKERSTATE state = CLICKERSTATE.IDLE;
+
+    public void GoIdle()
+    {
+        state = CLICKERSTATE.IDLE;
+    }
+
+    public void ClickObserve()
+    {
+        state = CLICKERSTATE.OBSERVE;
+    }
+
+    public void ClickUse()
+    {
+        state = CLICKERSTATE.USE;
+    }
+
+    public void ClickMenu()
+    {
+        state = CLICKERSTATE.MENU;
+    }
+
+    public void ClickMove()
+    {
+        state = CLICKERSTATE.MOVE;
+    }
+
+    public void ClickTalk()
+    {
+        state = CLICKERSTATE.TALK;
+    }
+
+    public void ClickPickUp()
+    {
+        state = CLICKERSTATE.PICKUP;
+    }
+}
