88f6d3b4de479241d23b6c3c32af1672f99804a8
Yohann Degli Esposti <yohann.degliesposti@gmail.com>
2021-01-23 13:06:46 +0100

    PNClick: added a first interface and other style settings.

---
+++ Assets/Scripts/PointNClick/PointObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface PointObject
+{
+    void setNameOnHover();
+
+    void PickUp();
+    void Use();
+    void Observe();
+    void Talk();
+}
